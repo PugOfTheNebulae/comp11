748badddbc10b598874028da837ea4a06e79e855
Alyssa Rose <32802037+alyssa-rose@users.noreply.github.com>
2018-10-27 10:47:04 -0400

    Update minesweeper_final.cpp

--- minesweeper_final.cpp
+++ minesweeper_final.cpp
@@ -39,172 +39,209 @@ that created different boards necessary to my program.
 */
 
 void print_board (char board[SIZE][SIZE]);
-//prints out a 2d array (board) to screen
-//input: board[SIZE][SIZE] of type char
 void first_board (char board[SIZE][SIZE]);
-//creates the first board printed to screen
-//input: board[SIZE][SIZE] of type char
 int hid_board (char board[SIZE][SIZE]);
-//creates the "hidden" board that the user
-//is using to play
-//input: board[SIZE][SIZE] of type char
-//output: number of mines in board (integer)
 int you_lost(char board[SIZE][SIZE]);
-//prints out board with mines and the phrase you lost
-//and returns the value 1 (outcome) which tells
-//the loop that it can end
 int you_won(char board[SIZE][SIZE]);
-//prints out board with num mines in vicinity and 
-//mphrase you won and returns the value 1 (outcome)
-// which tells the loop that it can end
 int num_mines (int row, int col, char board[SIZE][SIZE]);
-//counts the number of mines in the vicinity
-//input: row and col numbers played, board[SIZE][SIZE] 
-//		 of type char
-//output: number of mines in the vicinity
 bool wrong_dim(int row, int col);
-//prints out error message if the dimensions
-//entered are not 0 to 8 and returns false if
-//the dimensions are out of bounds
 void choose_again (int row, int col, char board[SIZE][SIZE]);
-//checks if user already selected the square and
-//returns error message if they have
+
+
 int main(){
-	//instance of data structure (type array)
-	char board_print[SIZE][SIZE];
-	char board_actual[SIZE][SIZE];
-	first_board(board_print);
-	print_board(board_print);
-	int num_x = hid_board(board_actual);
-	int outcome = 0, mine, tries = 0;
+    //instance of data structure (type 2D array)
+    char board_print[SIZE][SIZE];
+    char board_actual[SIZE][SIZE];
+    first_board(board_print);
+    print_board(board_print);
+    int num_x = hid_board(board_actual);
+    int outcome = 0, mine, tries = 0;
 
-	while (outcome == 0){
-		int row, col;
-		cout << "Enter the row and column of the square to uncover: ";
-		cin >> row;
-		cin >> col;
-		choose_again (row, col, board_print);
-		wrong_dim (row, col);
-		if (board_actual[row][col] == MINE){
-			outcome = you_lost(board_actual);
-		}
+    while (outcome == 0){
+        int row, col;
+        cout << "Enter the row and column of the square to uncover: ";
+        cin >> row;
+        cin >> col;
+        //checks if input valid in 2 ways
+        choose_again (row, col, board_print);
+        wrong_dim (row, col);
+        if (board_actual[row][col] == MINE){
+            outcome = you_lost(board_actual);
+        }
 
-		else {
-			mine = num_mines(row, col, board_actual);
-			mine += '0';
-			board_print[row][col] = mine;
-			print_board(board_print);
-			tries++;
-			if (tries == ((SIZE*SIZE) - num_x)){
-				outcome = you_won(board_print);
-			}
-		}
-	}
-	return 0;
+        else {
+            mine = num_mines(row, col, board_actual);
+            mine += '0';
+            board_print[row][col] = mine;
+            print_board(board_print);
+            tries++;
+            if (tries == ((SIZE*SIZE) - num_x)){
+                outcome = you_won(board_print);
+            }
+        }
+    }
+    return 0;
 }
 
+
+//int hid_board (char board[SIZE][SIZE]);
+//Purpose: creates the "hidden" board that the user
+//          is using to play against
+//Parameters: board[SIZE][SIZE] of type char
+//Returns: number of mines in board (integer)
+//Notes: return value is used to determine when
+//       user has won
 int hid_board (char board[SIZE][SIZE]){
-	srand(time(NULL));
-	int tries = 0; 
-	for (int i = 0; i < SIZE; i++){
-		for (int j = 0; j < SIZE; j++){
-			bool prob = rand() % 10 < 3;
-			if (prob){
-				board[i][j] = MINE;
-				tries++;
-			}
-			else
-				board[i][j] = UNSEEN;
-		}
-	}
-	return tries;
+    srand(time(NULL));
+    int tries = 0; 
+    for (int i = 0; i < SIZE; i++){
+        for (int j = 0; j < SIZE; j++){
+            cout << rand() % 10 << end;
+            bool prob = rand() % 10 < 3;
+            if (prob){
+                board[i][j] = MINE;
+                tries++;
+            }
+            else
+                board[i][j] = UNSEEN;
+        }
+    }
+    return tries;
 }
 
 
+//void first_board (char board[SIZE][SIZE]);
+//Purpose: creates the first board printed to screen
+//Parameters: board[SIZE][SIZE] of type char
+//Returns: None
+//Effects: None
+//Notes: used with print_board to print to
+//       standard output
 void first_board (char board[SIZE][SIZE]){
-	for (int i = 0; i < SIZE; i++){
-		for (int j = 0; j < SIZE; j++){
-			board[i][j] = UNSEEN;
-		}
-	}
+    for (int i = 0; i < SIZE; i++){
+        for (int j = 0; j < SIZE; j++){
+            board[i][j] = UNSEEN;
+        }
+    }
 }
 
 
+//void print_board (char board[SIZE][SIZE]);
+//Purpose: prints out a 2d array (board) to screen
+//Parameters: board[SIZE][SIZE] of type char
+//Returns: None
+//Effects: prints to standard output
+//Notes: None
 void print_board (char board[SIZE][SIZE]){
-	cout << "  0 1 2 3 4 5 6 7 8 \n";
-	for (int i = 0; i < SIZE; i++){
-		string m = " ";
-		for (int j = 0; j < SIZE; j++){
-			m = m + board[i][j] + " ";
-		}
-		cout << i << m << endl;
-		
-	}
+    cout << "  0 1 2 3 4 5 6 7 8 \n";
+    for (int i = 0; i < SIZE; i++){
+        string m = " ";
+        for (int j = 0; j < SIZE; j++){
+            m = m + board[i][j] + " ";
+        }
+        cout << i << m << endl;
+        
+    }
 }
 
 
+//int you_lost(char board[SIZE][SIZE]);
+//Purpose: prints out board with mines and 
+//          the phrase "GAME OVER. YOU LOST!"
+//Returns: the integer 1 (outcome)
+//Effects: results in while loop in main ending
+//Notes: only called when board[row][col] contains
+//       MINE, used in conjuction with print_board
 int you_lost(char board[SIZE][SIZE])
 {
-	print_board(board);
-	cout << "GAME OVER. YOU LOST!\n";
-	return 1;
+    print_board(board);
+    cout << "GAME OVER. YOU LOST!\n";
+    return 1;
 }
 
 
+//int you_won(char board[SIZE][SIZE]);
+//Purpose: prints out board with num mines in 
+//vicinity and phrase "GAME OVER. YOU WON!"
+//Returns: the integer 1 (outcome)
+//Effects: results in while loop in main ending
+//Notes: only called when all non MINE spaces
+//       have been chosen, used in conjuction
+//       with print_board
 int you_won(char board[SIZE][SIZE])
 {
-	print_board(board);
-	cout << "GAME OVER. YOU WON!\n";
-	return 1;
+    print_board(board);
+    cout << "GAME OVER. YOU WON!\n";
+    return 1;
 }
 
 
+//bool wrong_dim(int row, int col);
+//Purpose: prints out error message if the dimensions
+//         entered are not 0 to 8 
+//Returns: false if the dimensions are out of bounds
+//Effects: prints message to std output
+//Notes: None
 bool wrong_dim (int row, int col)
 {
-	if ((row > 8) or (row < 0) or (col < 0) or (col > 8)){
-		cout << "Bad input. Row and col must be >= 0 and < 9."
-		<< endl; 
-		return true;
-	}
-	else {
-		return false;
-	}
+    if ((row > 8) or (row < 0) or (col < 0) or (col > 8)){
+        cout << "Bad input. Row and col must be >= 0 and < 9."
+        << endl; 
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
 
+//void choose_again (int row, int col, char board[SIZE][SIZE]);
+//Purpose: checks if user already selected the square
+//Parameters: row and col numbers played, board[SIZE][SIZE] 
+//            of type char
+//Returns: None
+//Effects: prints message asking user to choose again
+//Notes: None
 void choose_again (int row, int col, char board[SIZE][SIZE])
 {
-	if (board[row][col] != UNSEEN){
-		if((row < 9) and (row >= 0) and (col >= 0) and (col < 9))
-			cout << "Bad input. Select another square." << endl;
-	}
+    if (board[row][col] != UNSEEN){
+        if((row < 9) and (row >= 0) and (col >= 0) and (col < 9))
+            cout << "Bad input. Select another square." << endl;
+    }
 }
 
 
+//int num_mines (int row, int col, char board[SIZE][SIZE]);
+//Purpose: counts the number of mines in the vicinity
+//Parameter: row and col numbers played, board[SIZE][SIZE] 
+//       of type char
+//Returns: number of mines in the vicinity
+//Effects: None
+//Notes: used in conjuction with print_board
 int num_mines (int row, int col, char board[SIZE][SIZE])
 {
-	int row_bk = row - 1;
-	if (row_bk < 0)
-		row_bk = 0;
+    int row_bk = row - 1;
+    if (row_bk < 0)
+        row_bk = 0;
 
-	int row_fwd = row + 1;
-	if (row_fwd > 8)
-		row_fwd = 8;
+    int row_fwd = row + 1;
+    if (row_fwd > 8)
+        row_fwd = 8;
 
-	int col_bk = col - 1;
-	if (col_bk < 0)
-		col_bk = 0;
+    int col_bk = col - 1;
+    if (col_bk < 0)
+        col_bk = 0;
 
-	int col_fwd = col + 1;
-	if (col_fwd > 8)
-		col_fwd = 8;
-	int cnt = 0;
+    int col_fwd = col + 1;
+    if (col_fwd > 8)
+        col_fwd = 8;
+    int cnt = 0;
 
-	for (int i = row_bk; i <= row_fwd; i++){
-		for (int j = col_bk; j <= col_fwd; j++){
-			if (board[i][j] == MINE)
-				cnt++;
-		}
-	}
-	return cnt;
+    for (int i = row_bk; i <= row_fwd; i++){
+        for (int j = col_bk; j <= col_fwd; j++){
+            if (board[i][j] == MINE)
+                cnt++;
+        }
+    }
+    return cnt;
 }
