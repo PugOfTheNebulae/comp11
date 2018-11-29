84bfe3b8a9da0d9d3ddf452fb72115ab5d8ebc11
Alyssa Rose <32802037+alyssa-rose@users.noreply.github.com>
2018-11-29 17:16:53 -0500

    Create highScoresList.h

---
+++ highScoresList.h
@@ -0,0 +1,37 @@
+#ifndef HIGHSCORESLIST_H
+#define HIGHSCORESLIST_H
+
+#include <string>
+
+class HighScoresList {
+    public:
+        HighScoresList();
+        ~HighScoresList();
+        void save();
+        int highestScore();
+        void print();
+        void printTop5();
+        void keepTop10();
+        void insert(std::string user, int score);
+        void clear();
+        void printUser(std::string user);
+        void deleteUser(std::string user);   // A JFFE, not required
+
+    private:
+        const std::string SENTINEL = "-1";
+        const std::string HIGH_SCORE_FILE = "highScores.txt";
+        struct Node
+        {
+            std::string user;
+            int val;
+            Node *next;
+        };
+        Node *head;
+
+        void load();
+        int length();
+        Node *after(int score);
+};
+
+
+#endif
