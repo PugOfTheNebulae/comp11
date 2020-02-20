4f5909142471e9f86191a22660be16a62b1e834c
Alyssa Rose <32802037+alyssa-rose@users.noreply.github.com>
2020-02-19 21:29:17 -0500

    Create golf.cpp

---
+++ lab1/golf.cpp
@@ -0,0 +1,78 @@
+/*
+ * golf.cpp
+ * purpose:      practice using booleans and constants
+ * modified by:  Alyssa Rose
+ * on date:      09-12-2018
+ */
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+// days of the week
+const int SUN = 0, MON = 1, TUE = 2, WED = 3, THR = 4, FRI = 5, SAT = 6;
+
+// constants for rules
+const double    BASE_PRICE            = 9.0;
+const int       HIGH_TEMP_CUTOFF      = 80;
+const int       LOW_TEMP_CUTOFF       = 65;
+const double    PER_DEGREE_PENALTY    = 0.10;
+const int       EVENING_START         = 17;
+const double    WEEKEND_EVE_SURCHARGE = 1.00;
+const double    RAIN_PENALTY          = 2.00;
+const int       WIND_GRANULARITY      = 15;
+const double    WIND_PENALTY          = 1.50;
+
+int main() 
+{
+        // -- define vars for input
+        int     tempF, dayOweek, hour, rainP, windMPH;
+
+        // -- a place to hold the price,
+        // -- to be initialized and updated
+        // -- based on the conditions below
+        double  price;
+
+        // -- set the precision used to output floating point numbers
+        cout << setiosflags(ios::fixed) << setprecision(2);
+        
+        // -- Get data from user
+        cout << "Temperature (degrees fahrenheit)? ";
+        cin  >> tempF;
+
+        cout << "Day of week (0 = Sun, 1 = Mon, ..., 6 = Sat)? " ;
+        cin  >> dayOweek;
+        
+        cout << "Hour of day (0..23)? ";
+        cin  >> hour;
+
+        cout << "Is it raining (0 = no, 1 = yes)? ";
+        cin  >> rainP;
+
+        cout << "Wind speed (MPH)? ";
+        cin  >> windMPH;
+
+        // -- compute price based on input values
+
+        price = BASE_PRICE;             
+	if (tempF > HIGH_TEMP_CUTOFF){
+	  price = BASE_PRICE - (PER_DEGREE_PENALTY)*(tempF - HIGH_TEMP_CUTOFF);
+	}
+	if (tempF < LOW_TEMP_CUTOFF){
+	  price = BASE_PRICE - (PER_DEGREE_PENALTY)*(tempF - LOW_TEMP_CUTOFF);
+	}
+	if ((dayOweek == SUN) || (dayOweek == SAT)){
+	    price++;
+	}
+	if (hour >= EVENING_START){
+	  price++;
+        }
+	if (rainP == 1){
+	  price = price - RAIN_PENALTY;
+	}
+        // -- report price to user
+
+        cout << "Price: " << price << endl;
+        return 0;
+}
